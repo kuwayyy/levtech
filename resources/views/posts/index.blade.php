e8ad6be154a352fbe4f10a4365960200991e417e
kuwayyy <kuwakatu07@gmail.com>
2021-11-28 20:42:19 +0900

    [add]ブログ投稿作成処理実行時のバリデーション実装

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,7 +8,7 @@
     </head>
     <body>
         <h1>Blog Name</h1>
-        <p class='create'>[<a href='/posts/create'>create</a>]</p>
+        <p class='create'>[<a href='/posts/create'>投稿作成</a>]</p>
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
