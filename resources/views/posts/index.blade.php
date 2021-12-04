72eb5090f796b88fc9c9f38121971aea2cdd3ff0
kuwayyy <kuwakatu07@gmail.com>
2021-12-04 16:17:03 +0900

    [add]ブログ投稿の削除

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -16,10 +16,23 @@
                     <p class='body'>{{ $post->body }}</p>
                 </div>
                 <p class="edit">[<a href="/posts/{{ $post->id }}/edit">編集</a>]</p>
+                <form action="/posts/{{ $post->id }}" id="form_delete" method="post" style="display:inline">
+                    @csrf
+                    @method('delete')
+                    <input type="submit" style="display:none">
+                    <p class='delete'>[<span onclick="return deletePost(this);">削除</span>]</p>
             @endforeach
         </div>
         <div class='paginate'>
             {{ $posts->links() }}
         </div>
+        <script>
+            function deletePost(e) {
+                'use strict';
+                if (confirm('削除すると復元できません。\n本当に削除しますか。')) {
+                    document.getElementById('form_delete').submit();
+                }
+            }
+        </script>
     </body>
 </html>
